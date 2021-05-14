bd7202530abfece557163de9f733104cb54b7055
Dylan Tisdall <mtisdall@pennmedicine.upenn.edu>
2021-05-14 21:16:08 +0000

    Updated MPSSFP_example to output useful data

--- building_blocks/MPSSFPTR.h
+++ building_blocks/MPSSFPTR.h
@@ -1,23 +1,28 @@
 #ifndef MPSSFPTR_h
 #define MPSSFPTR_h
 
+#include <array>
+
 template <typename SpinStates, unsigned int NumPulses>
 class MPSSFPTR {
   public:
     typedef typename SpinStates::value_type value_type;
 
     MPSSFPTR(
-      const value_type flipAngle,
+      const std::array<value_type, NumPulses> flipAngles,
       const value_type flipPhaseStep,
       const typename SpinStates::StateIndex spoilingGrad,
       const value_type interPulseTime,
       const value_type t1,
       const value_type t2,
+      const value_type offResonancePhaseStepPerInterPulse,
       const value_type threshold = value_type(0)
       ) :
-      flipAngle(flipAngle),
+      flipAngles(flipAngles),
       flipCurPhase(0),
       flipPhaseStep(flipPhaseStep),
+      curOffResonancePhase(0),
+      offResonancePhaseStep(offResonancePhaseStepPerInterPulse),
       spoiler(spoilingGrad, interPulseTime, threshold),
       relaxation(interPulseTime, t1, t2)
     {}
@@ -25,7 +30,9 @@ class MPSSFPTR {
     typename SpinStates::cvalue_type operator()(SpinStates *states) {
       for(unsigned int i = 0; i < NumPulses; i++) {
         { 
-          typename SpinStates::Excitation excitation(flipAngle, flipCurPhase);
+          typename SpinStates::Excitation excitation(
+              flipAngles[i], flipCurPhase + curOffResonancePhase);
+          
           flipCurPhase += flipPhaseStep;
           while(flipCurPhase > value_type(2.0 * M_PI) ) {
             flipCurPhase -= value_type(2.0 * M_PI);
@@ -33,15 +40,33 @@ class MPSSFPTR {
           while(flipCurPhase < value_type(-2.0 * M_PI) ) {
             flipCurPhase += value_type(2.0 * M_PI);
           }
+          
+          curOffResonancePhase += offResonancePhaseStep;
+          while(curOffResonancePhase > value_type(2.0 * M_PI) ) {
+            curOffResonancePhase -= value_type(2.0 * M_PI);
+          }
+          while(curOffResonancePhase < value_type(-2.0 * M_PI) ) {
+            curOffResonancePhase += value_type(2.0 * M_PI);
+          }
 
           excitation(states);
         }
+        
         spoiler(states);
         relaxation(states);
       } 
 
       typename SpinStates::cvalue_type ret =
         states->getState(typename SpinStates::StateIndex({{0,0,0}})).fPlus;
+      
+      curOffResonancePhase += offResonancePhaseStep;
+      while(curOffResonancePhase > value_type(2.0 * M_PI) ) {
+        curOffResonancePhase -= value_type(2.0 * M_PI);
+      }
+      while(curOffResonancePhase < value_type(-2.0 * M_PI) ) {
+        curOffResonancePhase += value_type(2.0 * M_PI);
+      }
+
       spoiler(states);
       relaxation(states); 
 
@@ -50,9 +75,11 @@ class MPSSFPTR {
 
 
   protected:
-    value_type flipAngle;
+    std::array<value_type, NumPulses> flipAngles;
     value_type flipCurPhase;
     value_type flipPhaseStep;
+    value_type curOffResonancePhase;
+    value_type offResonancePhaseStep;
     typename SpinStates::Spoiling spoiler;
     typename SpinStates::Relaxation relaxation;
 };
--- examples/MPSSFP_example.cpp
+++ examples/MPSSFP_example.cpp
@@ -16,30 +16,45 @@ void test() {
   typedef SpinStates<long, value_type> SpinStatesT;
   SpinStatesT spinStates(10, 2.2e-3);
 
-  MPSSFPTR<SpinStatesT, 2> mpssfpTR(
-    value_type(10) * value_type(M_PI) / value_type(180),
-    0,
-    SpinStatesT::StateIndex({{1,0,0}}),
-    4,
-    1000,
-    100,
-    1e-4);
+  std::array<value_type, 3> flipAngles;
 
-  { 
-    SpinStatesT::cvalue_type excitedState = mpssfpTR(&spinStates); 
+  for(unsigned int fa = 1; fa < 180; fa++) {
+    flipAngles.fill(value_type(fa) * value_type(M_PI) / value_type(180));
+    //flipAngles[0] = value_type(10) * value_type(M_PI) / value_type(180);
+    //flipAngles[1] = value_type(30) * value_type(M_PI) / value_type(180);
+    //flipAngles[2] = value_type(10) * value_type(M_PI) / value_type(180);
 
-    std::cout << "MPSSFP TR signal: " << excitedState << std::endl;
-    
-    std::cout << "after MPSSFP TR:" << std::endl;
-    for(int i = -1; i <= 1; i++) {
-      SpinStatesT::StateIndex curIndex({{i,0,0}});
-      SpinStatesT::SpinState curState = spinStates.getState(curIndex);
-     
-      std::cout << "\t(" << curIndex[0] << ", "
-        << curIndex[1] << ", " << curIndex[2] << "): (" <<
-        curState.fPlus << ", " <<
-        curState.fMinus << ", " <<
-        curState.z << ")" << std::endl;
+    MPSSFPTR<SpinStatesT, 3> mpssfpTR(
+      flipAngles,
+      //0,
+      value_type(180) * value_type(M_PI) / value_type(180),
+      SpinStatesT::StateIndex({{1,0,0}}),
+      4,
+      650,
+      80,
+      value_type(0) * value_type(M_PI) / value_type(180),
+      1e-4);
+
+    {
+      for(unsigned int i = 0; i < 1000; i++) {
+        mpssfpTR(&spinStates);
+      }
+
+      SpinStatesT::cvalue_type excitedState = mpssfpTR(&spinStates); 
+
+      std::cout << ", " << std::abs(excitedState) << std::endl;
+      
+      //std::cout << "after MPSSFP TR:" << std::endl;
+      //for(int i = -1; i <= 1; i++) {
+      //  SpinStatesT::StateIndex curIndex({{i,0,0}});
+      //  SpinStatesT::SpinState curState = spinStates.getState(curIndex);
+      // 
+      //  std::cout << "\t(" << curIndex[0] << ", "
+      //    << curIndex[1] << ", " << curIndex[2] << "): (" <<
+      //    curState.fPlus << ", " <<
+      //    curState.fMinus << ", " <<
+      //    curState.z << ")" << std::endl;
+      //}
     }
   }
 }
